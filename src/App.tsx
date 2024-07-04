bf54f5c3eb8715bd7e7eccb86da1c710d3b76417
islamhafez0 <islamhafez872@gmail.com>
2024-07-05 01:06:53 +0300

    remove consoles

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,6 @@ import Header from "./Header";
 const App = () => {
   const [currentUser, setCurrentUser] = useState<null | User>(null);
   const [gettingCurrentUser, setGettingCurrentUser] = useState(true);
-  console.log(process.env.REACT_APP_API_KEY);
-  console.log(process.env.REACT_APP_AUTH_DOMAIN);
-  console.log(process.env.REACT_APP_PROJECT_ID);
-  console.log(process.env.REACT_APP_STORAGE_BUCKET);
-  console.log(process.env.REACT_APP_MESSAGING_SENDER_ID);
-  console.log(process.env.REACT_APP_ID);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setGettingCurrentUser(false);
